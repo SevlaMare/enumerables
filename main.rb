35d69393f7c6af9e4901a2be7a76c8af11081897
SevlaMare <sevilamare@gmail.com>
2020-03-25 19:22:48 -0300

    fix my_each length

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 module Enumerable
   def my_each
-    for count in 0..length
+    for count in 0..length-1
       yield(self[count])
     end
   end
 
   def my_each_with_index
-    for count in 0..length
+    for count in 0..length-1
       yield(self[count], count)
     end
   end

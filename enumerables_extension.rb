245eb594b286f0185ccf518c570f792a4bf25dc9
SevlaMare <sevilamare@gmail.com>
2020-03-25 16:25:13 -0300

    setup

--- enumerables_extension.rb
+++ enumerables_extension.rb
@@ -3,3 +3,16 @@ module Enumerable
     # your code here
   end
 end
+
+class Array 
+  def my_each
+    i = 0
+    while i < self.size
+        yield(self[i])  
+        i+=1      
+    end
+    self
+  end
+end
+
+[1,2,3].my_each { |i| puts i}

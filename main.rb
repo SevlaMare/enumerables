beafc366f25053fa2bae15bb71e0f7c8e19575b4
SevlaMare <sevilamare@gmail.com>
2020-03-26 15:05:28 -0300

    litner fixes

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 module Enumerable
   def my_each
-    for count in 0..length-1
+    for count in 0..length - 1
       yield(self[count])
     end
   end
 
   def my_each_with_index
-    for count in 0..length-1
+    for count in 0..length - 1
       yield(self[count], count)
     end
   end
@@ -16,7 +16,34 @@ module Enumerable
     for i in self
       filter.push(i) if yield(i)
     end
-    return filter
+    filter
+  end
+
+  def my_all?
+    # future
+  end
+
+  def my_any?
+    # future
+  end
+
+  def my_none?
+    # future
+  end
+
+  def my_count
+    # future
+  end
+
+  def my_map
+    # future
+  end
+
+  def my_inject
+    # future
+  end
+
+  def multiply_els
+    # future
   end
 end
-

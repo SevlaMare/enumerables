3a725dfb8a3f2e3979709024122d205b55920f7d
SevlaMare <sevilamare@gmail.com>
2020-03-26 13:23:34 -0300

    added my_select

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 module Enumerable
   def my_each
-    for count in self
+    for count in 0..length-1
       yield(self[count])
     end
   end
@@ -10,4 +10,13 @@ module Enumerable
       yield(self[count], count)
     end
   end
+
+  def my_select
+    filter = []
+    for i in self
+      filter.push(i) if yield(i)
+    end
+    return filter
+  end
 end
+

70b0d567c9361382e89eff95c61826cf7ba91607
SevlaMare <sevilamare@gmail.com>
2020-04-07 21:00:26 -0300

    added 6 test for my_each

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,14 +2,40 @@ require 'rspec/autorun'
 require_relative '../main.rb'
 
 RSpec.describe Enumerable do
-  # variables to all tests
-  let(:array_positive) { [1, 2, 3] }
+  let(:array_numeric) { [-1, 0, 1] }
+  let(:array_string) { %w[aaa bb 123 c] }
+
+  let(:range_forward) { (1..3) }
+  let(:array_unique) { [1] }
+
+  let(:empty_block) { }
 
   describe '#my_each' do
-    # CASE 1 ( can  have multiples it)
-    it 'iterates through array elements' do
-      expect(array_positive.my_each { |element| element })
-        .to eql(array_positive.each { |element| element })
+    context 'Iterates through an array of numbers' do
+      it 'Should return the same array of input' do
+        expect(array_numeric.my_each { |element| element })
+          .to eql(array_numeric.each { |element| element })
+      end
+    end
+
+    it 'iterates through array of strings' do
+      expect(array_string.my_each { |element| element })
+        .to eql(array_string.each { |element| element })
+    end
+
+    it 'iterates through a range' do
+      expect(range_forward.my_each { |x| x })
+        .to eql(range_forward.to_a)
+    end
+
+    it 'array with only one element' do
+      expect(array_unique.my_each { |x| x })
+        .to eql(array_unique.each { |element| element })
+    end
+
+    it 'If no block is given, an enumerator is returned instead.' do
+      expect(array_numeric.my_each)
+        .to be_a(Enumerable)
     end
   end
 end

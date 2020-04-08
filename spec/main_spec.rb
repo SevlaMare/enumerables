5534d27743de2552e7bb756742ed8bc604df2ae1
SevlaMare <sevilamare@gmail.com>
2020-04-07 21:36:36 -0300

    linter fixes

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -8,19 +8,17 @@ RSpec.describe Enumerable do
   let(:range_forward) { (1..3) }
   let(:array_unique) { [1] }
 
-  let(:empty_block) { }
-
   describe '#my_each' do
     context 'Iterates through an array of numbers' do
       it 'Should return the same array of input' do
         expect(array_numeric.my_each { |element| element })
-          .to eql(array_numeric.each { |element| element })
+          .to eql(array_numeric)
       end
     end
 
     it 'iterates through array of strings' do
       expect(array_string.my_each { |element| element })
-        .to eql(array_string.each { |element| element })
+        .to eql(array_string)
     end
 
     it 'iterates through a range' do
@@ -30,10 +28,10 @@ RSpec.describe Enumerable do
 
     it 'array with only one element' do
       expect(array_unique.my_each { |x| x })
-        .to eql(array_unique.each { |element| element })
+        .to eql(array_unique)
     end
 
-    it 'If no block is given, an enumerator is returned instead.' do
+    it 'If no block is given, an enumerator is returned.' do
       expect(array_numeric.my_each)
         .to be_a(Enumerable)
     end
